f289bf8e40fcf3cfe31f61a11d2d5a7aceb63448
Giovanni Carvalho de Oliveira <120597406+gioCarv@users.noreply.github.com>
2022-12-14 16:32:32 -0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,87 @@
+const {authSecret} = require('./.env')
+// const jwt = require('jwt-simple')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const mysql = require("mysql")
+const db = mysql.createPool({
+    host:'localhost',
+    user: 'root',
+    password:'1234',
+    database:'netflix',
+})
+
+
+
+
+
+    const signin = async (req, res) => {
+        if (!req.body.email && !req.body.password){
+            return res.status(400).send('informe usuário e senha')
+        }
+        
+        
+        let user;
+
+        function createUserToken(value) {
+            user = value;
+            console.log(user.email, 'Logando com sucesso');
+            res.json({
+                ...user,
+                token: jwt.sign(user, authSecret)   
+            })
+        }
+
+
+        
+        const sqlSelect =
+            "SELECT * from users WHERE email = ?  AND password = ?"
+            db.query(sqlSelect,[req.body.email, req.body.password], (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(result[0].idusers,'result id')
+                const now =  Math.floor(Date.now() / 1000)
+
+                const userInfo = {
+                id:result[0].idusers ,
+                email:result[0].email ,
+                iat: now,
+                exp: now + (60 * 60 * 24 * 3),
+            }
+
+            createUserToken(userInfo);
+
+            }
+            
+        })
+
+    }
+    
+    
+    const validateToken = async (req, res, next) => {
+        const tokenAndUser = req.body.token || null
+        try{
+            console.log(tokenAndUser)
+            if(tokenAndUser){
+                if(tokenAndUser.exp > Math.floor(Date.now() / 1000)) {
+                    jwt.verify(tokenAndUser.token, authSecret, (err, decoded)=>{
+                        if (err) return res.status(401)
+                        // if (err) return res.status(401).end()
+                    })
+                    return await res.send(true)
+                }else{
+                    return await res.send(false)
+                }
+            }
+        } catch(e){
+            console.log(e)
+        }
+        
+        
+        console.log('FINALIZAMOS errado')
+    
+        res.send(false)
+}
+    module.exports = { signin, validateToken }
+
+
---
+++ index.js
@@ -0,0 +1,112 @@
+const express = require("express")
+const app = express()
+const mysql = require("mysql")
+const cors = require("cors")
+const bodyParser = require("body-parser");
+const auth = require('./auth')
+const port = process.env.PORT || 3002
+
+
+const db = mysql.createPool({
+    host:'localhost',
+    user: 'root',
+    password:'1234',
+    database:'netflix',
+})
+
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.post('/signup',(req, res) => {
+    const {email} = req.body
+    const {password} = req.body
+
+    let sqlInsert =
+    "INSERT INTO users (email, password) VALUES (?, ?);";
+    db.query(sqlInsert, [email, password], (err,result) => {
+        if (err) console.log(err)
+    })
+})
+
+
+
+ app.get( '/get/', (req,res)=> {
+    const sqlSelect = 
+    "SELECT email from users "
+    db.query(sqlSelect, (err,result) =>{
+        if (err) console.log(err)      
+        res.send(result)
+    })
+})
+
+app.get( '/get/:email', (req,res)=> {
+    const email = req.params.email
+    const sqlSelect = 
+    "SELECT email from users WHERE email = ?"
+    db.query(sqlSelect, email, (err,result) =>{
+        console.log(result)
+        if (err) console.log(err)
+       res.send(result)
+   })
+})
+
+
+app.post( '/signin', auth.signin)
+
+app.post( '/home', auth.validateToken)
+
+
+app.post( '/userEmail', (req,res)=> {
+    const email = req.body.email
+    
+    const sqlSelectEmail = 
+    "SELECT email from users WHERE email = ?"
+    
+    db.query(sqlSelectEmail, [email], (err,result) =>{
+        if (err) console.log(err)
+        try{
+            
+        if (result[0].email){
+              res.send(true)
+        }}catch{
+            res.send(false)
+        }
+        
+    })
+})
+
+
+
+app.post( '/user', (req,res)=> {
+    const email = req.body.email
+    const password = req.body.password
+
+
+
+    const sqlSelect =    
+    "SELECT email from users WHERE email = ? AND password = ?"
+    db.query(sqlSelect, [email, password], (err,result) =>{
+        if (err) console.log(err)
+        try{
+            if(result[0].email){
+                res.send(true)
+            }
+        }catch{
+            res.send(false)            
+        }
+        
+        })
+    })
+
+
+
+    
+
+
+
+app.listen(port, ()=> {
+    console.log(`Rodando na porta 3002`)
+})
